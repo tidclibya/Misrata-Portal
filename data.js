60d1f6bd7d8c082a50efb28f71188a63b6b96074
tidclibya <tidclibya@gmail.com>
2026-02-16 14:51:34 +0200

    Refactor and expand Misrata data structure

--- data.js
+++ data.js
@@ -1,142 +1,399 @@
-// تهيئة الخريطة في وسط مصراتة
-var map = L.map('map').setView([32.374, 15.092], 13);
-
-// إضافة طبقة الخريطة (OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-// تجميع العلامات لتجنب الازدحام
-var markersCluster = L.markerClusterGroup({
-    showCoverageOnHover: false,
-    zoomToBoundsOnClick: true,
-    spiderfyOnMaxZoom: true
-});
-
-// ألوان الفئات - متناسقة مع الهوية (ذهبي، أزرق، أخضر)
-var categoryColors = {
-    restaurants: '#fbbf24', // ذهبي
-    hotels: '#1e3a8a',      // أزرق داكن
-    resorts: '#10b981'      // أخضر (للتمييز)
-};
-
-// حفظ جميع العلامات للتصفية
-var allMarkers = [];
-
-// دوال مساعدة لإنشاء محتوى النافذة المنبثقة حسب الفئة
-function getPopupContent(item, category) {
-    let content = `<strong>${item.name}</strong><br>`;
-    
-    if (category === 'restaurants') {
-        content += `📍 ${item.address || 'غير متوفر'}<br>`;
-        content += `📞 ${item.phone || 'غير متوفر'}<br>`;
-        if (item.facebook) {
-            content += `<a href="${item.facebook}" target="_blank">📱 صفحة فيسبوك</a><br>`;
-        }
-    } else if (category === 'hotels') {
-        content += `🏨 ${item.classification || ''}<br>`;
-        content += `🛏️ غرف: ${item.rooms || 'غير متوفر'}<br>`;
-        content += `📞 ${item.phone || 'غير متوفر'}<br>`;
-        if (item.website) {
-            content += `<a href="http://${item.website}" target="_blank">🌐 الموقع الإلكتروني</a><br>`;
-        }
-    } else if (category === 'resorts') {
-        content += `🏝️ ${item.classification || ''}<br>`;
-        content += `🏠 شاليهات: ${item.chalets || 'غير متوفر'}<br>`;
-        content += `📞 ${item.phone || 'غير متوفر'}<br>`;
-        if (item.beach) content += `🏖️ شاطئ: نعم<br>`;
+var misrataData = {
+  // المطاعم (30)
+  "restaurants": [
+    {
+      "id": 1,
+      "name": "مطعم سميني انت",
+      "lat": 32.37394,
+      "lng": 15.08173,
+      "address": "شارع البيرة",
+      "phone": "922118685",
+      "facebook": "https://www.facebook.com/smeneant/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 2,
+      "name": "مطعم كودو",
+      "lat": 32.35397,
+      "lng": 15.09945,
+      "address": "شارع بنغازي",
+      "phone": "091-3930300",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D9%83%D9%88%D8%AF%D9%88-%D9%85%D8%B5%D8%B1%D8%A7%D8%AA%D8%A9-100094945942436/",
+      "type": "مطعم"
+    },
+    {
+      "id": 3,
+      "name": "مطعم الحرية للمأكولات التركية",
+      "lat": 32.3242,
+      "lng": 15.11087,
+      "address": "الطريق الساحلي",
+      "phone": "092-5220208",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D8%A7%D9%84%D8%AD%D8%B1%D9%8A%D8%A9-%D9%84%D9%84%D9%85%D8%A3%D9%83%D9%88%D9%84%D8%A7%D8%AA-%D8%A7%D9%84%D8%A8%D8%AD%D8%B1%D9%8A%D9%87-100066746681354/",
+      "type": "مطعم"
+    },
+    {
+      "id": 4,
+      "name": "برجر هاوس",
+      "lat": 32.36877,
+      "lng": 15.0825,
+      "address": "شارع طرابلس",
+      "phone": "091-4391000",
+      "facebook": "https://www.facebook.com/burgerhousemisurata/",
+      "type": "مطعم"
+    },
+    {
+      "id": 5,
+      "name": "مطعم الراقي",
+      "lat": 32.40889,
+      "lng": 15.03035,
+      "address": "الجزيرة",
+      "phone": "091-3834002",
+      "facebook": "https://www.facebook.com/people/%D9%85%D8%B7%D8%B9%D9%85-%D8%A7%D9%84%D8%B1%D8%A7%D9%82%D9%8A-Alraqi-Resturant/100054569165310/",
+      "type": "مطعم"
+    },
+    {
+      "id": 6,
+      "name": "شاورما كورنر",
+      "lat": 32.36824,
+      "lng": 15.07508,
+      "address": "فرع الإذاعة",
+      "phone": "091-6230731",
+      "facebook": "https://www.facebook.com/people/%D8%B4%D8%A7%D9%88%D8%B1%D9%85%D8%A7-%D9%83%D9%88%D8%B1%D9%86%D8%B1/100064616324679/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 7,
+      "name": "مطعم لاقونا",
+      "lat": 32.37437,
+      "lng": 15.08986,
+      "address": "مصراتة",
+      "phone": "910800016",
+      "facebook": "https://www.facebook.com/lagunaa2022?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 8,
+      "name": "مطعم أهلا وسهلا2",
+      "lat": 32.37377,
+      "lng": 15.08904,
+      "address": "شارع طرابلس",
+      "phone": "954333333",
+      "facebook": "https://www.facebook.com/ahilanwasahlanResturant2022/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 9,
+      "name": "مطعم السنابل",
+      "lat": 32.36304,
+      "lng": 15.10376,
+      "address": "طريق دائري الرويصات بالقرب من صالة المنى",
+      "phone": "091-7403945",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D8%A7%D9%84%D8%B3%D9%86%D8%A7%D8%A8%D9%84-100057051326673/",
+      "type": "مطعم"
+    },
+    {
+      "id": 10,
+      "name": "مطعم غرناطة",
+      "lat": 32.37416,
+      "lng": 15.08951,
+      "address": "شارع طرابلس بالقرب من جزيرة العلم",
+      "phone": "925715411",
+      "facebook": "https://www.facebook.com/grnata98/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 11,
+      "name": "مشاوي Sizzle",
+      "lat": 32.36353,
+      "lng": 15.07575,
+      "address": "شارع طرابلس .. مقابل مصرف الجمهورية",
+      "phone": "092-2600605",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B4%D8%A7%D9%88%D9%8A-%D8%B3%D9%8A%D8%B2%D9%84-100057174831096/",
+      "type": "مطعم"
+    },
+    {
+      "id": 12,
+      "name": "مطعم grill",
+      "lat": 32.35638,
+      "lng": 15.09972,
+      "address": "شــارع بنغــازي مقـابـل فنــدق المــاسة 2",
+      "phone": "091-3016290",
+      "facebook": "https://www.facebook.com/grillmisrata/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 13,
+      "name": "مطعم لافينو",
+      "lat": 32.37404,
+      "lng": 15.09031,
+      "address": "شارع طرابلس",
+      "phone": "918930033",
+      "facebook": "https://www.facebook.com/Lavino.misurata/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 14,
+      "name": "مطعم السلطان",
+      "lat": 32.36927,
+      "lng": 15.1416,
+      "address": "بالقرب من جزيرة الزروق",
+      "phone": "913132414",
+      "facebook": "https://www.facebook.com/AlSoultanRes/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 15,
+      "name": "Alanya grill ألانيا قريل",
+      "lat": 32.36634,
+      "lng": 15.89684,
+      "address": "(شارع بنغازي)",
+      "phone": "091-3686006",
+      "facebook": "https://www.facebook.com/p/%D8%A7%D9%84%D8%A7%D9%86%D9%8A%D8%A7-%D9%82%D8%B1%D9%8A%D9%84-%D9%85%D8%B5%D8%B1%D8%A7%D8%AA%D9%87-Alanya-Grill-Misrata-100069280620814/",
+      "type": "مطعم"
+    },
+    {
+      "id": 16,
+      "name": "شوايا ستيك هاوس",
+      "lat": 32.35374,
+      "lng": 15.09946,
+      "address": "شارع بنغازي",
+      "phone": "921730111",
+      "facebook": "https://www.facebook.com/shwayamisrata/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 17,
+      "name": "بيتزا أكاكوس الفرع الرئيسي",
+      "lat": 32.3604,
+      "lng": 15.09682,
+      "address": "سيمافرو شارع الهلال",
+      "phone": "091-3886769",
+      "facebook": "https://www.facebook.com/PizzaAcacus/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 18,
+      "name": "مطعم ساسو",
+      "lat": 32.34838,
+      "lng": 15.04417,
+      "address": "طريق المطار",
+      "phone": "091-6136661",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D8%B3%D8%A7%D8%B3%D9%88-61556753909132/",
+      "type": "مطعم"
+    },
+    {
+      "id": 19,
+      "name": "مطعم أميقوس",
+      "lat": 32.36517,
+      "lng": 15.05077,
+      "address": "مصراتة_طريق_الصوالح",
+      "phone": "091-8447755",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D8%A7%D9%85%D9%8A%D9%82%D9%88%D8%B3-Amigos-Restaurant-100064692352294/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 20,
+      "name": "مطعم مارينا للمأكولات البحرية",
+      "lat": 32.40856,
+      "lng": 15.03103,
+      "address": "منطقة الجزيرة بالقرب من مناره مول",
+      "phone": "092-0565666",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D9%85%D8%A7%D8%B1%D9%8A%D9%86%D8%A7-%D9%84%D9%84%D9%85%D8%A3%D9%83%D9%88%D9%84%D8%A7%D8%AA-%D8%A7%D9%84%D8%A8%D8%AD%D8%B1%D9%8A%D9%87-100082917203268/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 21,
+      "name": "Mayo Burger",
+      "lat": 32.37458,
+      "lng": 15.0901,
+      "address": "شارع طرابلس بجانب سارية العلم",
+      "phone": "092-2127012",
+      "facebook": "https://www.facebook.com/Mayo.misrata/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 22,
+      "name": "دجاج كنتاكي",
+      "lat": 32.36459,
+      "lng": 15.07777,
+      "address": "شارع طرابلس - بجوار بيلاميا للجيلاطي الايطالي",
+      "phone": "091-5929059",
+      "facebook": "",
+      "type": "مطعم"
+    },
+    {
+      "id": 23,
+      "name": "مطعم إزمير",
+      "lat": 32.37852,
+      "lng": 14.99267,
+      "address": "زاوية المحجوب مقابل مدرسة أبوبكر الصديق",
+      "phone": "091-8653915",
+      "facebook": "https://www.facebook.com/Ankara2Libya?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 24,
+      "name": "مطعم الراقي",
+      "lat": 32.40889,
+      "lng": 15.03034,
+      "address": "الجزيرة بالقرب من جزيرة دوران المصايف",
+      "phone": "091-3834002",
+      "facebook": "https://www.facebook.com/people/%D9%85%D8%B7%D8%B9%D9%85-%D8%A7%D9%84%D8%B1%D8%A7%D9%82%D9%8A-Alraqi-Resturant/100054569165310/",
+      "type": "مطعم"
+    },
+    {
+      "id": 25,
+      "name": "مطعم لافينو",
+      "lat": 32.37403,
+      "lng": 15.09032,
+      "address": "شارع طرابلس قرب سارية العلم",
+      "phone": "918930033",
+      "facebook": "https://www.facebook.com/Lavino.misurata/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 26,
+      "name": "مطعم الفيصل",
+      "lat": 32.36209,
+      "lng": 15.09953,
+      "address": "شارع بنغازي",
+      "phone": "092-8197141",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D8%A7%D9%84%D9%81%D9%8A%D8%B5%D9%84-100064031174914/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 27,
+      "name": "مطعم الجرة",
+      "lat": 32.35044,
+      "lng": 15.16795,
+      "address": "طريق النقل الثقيل مقابل فتحة مصنع النسيم",
+      "phone": "923152022",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D8%A7%D9%84%D8%AC%D8%B1%D8%A9-%D9%85%D8%B5%D8%B1%D8%A7%D8%AA%D8%A9-100092382466031/",
+      "type": "مطعم"
+    },
+    {
+      "id": 28,
+      "name": "مطعم فريسكو",
+      "lat": 32.37457,
+      "lng": 15.07098,
+      "address": "مقابل جزيرة النهر (المهمل)",
+      "phone": "922423468",
+      "facebook": "https://www.facebook.com/p/%D9%85%D8%B7%D8%B9%D9%85-%D9%81%D8%B1%D9%8A%D8%B3%D9%83%D9%88-%D9%85%D8%B5%D8%B1%D8%A7%D8%AA%D8%A9-100064177624403/?locale=ar_AR",
+      "type": "مطعم"
+    },
+    {
+      "id": 29,
+      "name": "مطعم طشطشة",
+      "lat": 32.37343,
+      "lng": 15.08929,
+      "address": "شارع طرابلس",
+      "phone": "092-2223722",
+      "facebook": "https://www.facebook.com/p/Tashtasha-misrata-61556594934447/",
+      "type": "مطعم"
+    },
+    {
+      "id": 30,
+      "name": "مطعم شيشليك",
+      "lat": 32.36689,
+      "lng": 15.08106,
+      "address": "شارع طرابلس بي جانب معرض الشهداء",
+      "phone": "091-7560227",
+      "facebook": "https://www.facebook.com/p/%D8%B4%D9%8A%D8%B4%D9%84%D9%8A%D9%83-shishlik-100064974970103/?locale=ar_AR",
+      "type": "مطعم"
     }
-    
-    // إضافة زر الاتجاهات
-    content += `<a href="https://www.google.com/maps/dir/?api=1&destination=${item.lat},${item.lng}" target="_blank">🗺️ الاتجاهات</a>`;
-    
-    return content;
-}
+  ],
 
-// إضافة جميع العلامات من البيانات
-function addMarkers() {
-    for (let category in misrataData) {
-        misrataData[category].forEach(item => {
-            // إنشاء رمز مخصص
-            let marker = L.marker([item.lat, item.lng], {
-                icon: L.divIcon({
-                    className: 'custom-marker',
-                    html: `<div style="background-color: ${categoryColors[category]}; width: 16px; height: 16px; border-radius: 50%; border: 2px solid white; box-shadow: 0 2px 5px rgba(0,0,0,0.3);"></div>`,
-                    iconSize: [20, 20]
-                })
-            });
-            
-            marker.category = category;
-            marker.itemData = item;
-            marker.bindPopup(getPopupContent(item, category));
-            
-            allMarkers.push(marker);
-            markersCluster.addLayer(marker);
-        });
+  // الفنادق
+  "hotels": [
+    {
+      "id": 1,
+      "name": "فندق اليافا",
+      "name_en": "Yafa Hotel",
+      "classification": "4 نجوم",
+      "rooms": 80,
+      "beds": 160,
+      "suites": 10,
+      "ownership": "خاص",
+      "established": 2005,
+      "phone": "0910000300",
+      "email": "",
+      "website": "www.yafa.ly",
+      "parking": true,
+      "conference": true,
+      "restaurant": true,
+      "rating": 4.2,
+      "lat": 32.368,
+      "lng": 15.099,
+      "image": "",
+      "notes": "فندق معروف"
+    },
+    {
+      "id": 2,
+      "name": "فندق التاج",
+      "name_en": "Al Taj Hotel",
+      "classification": "3 نجوم",
+      "rooms": 60,
+      "beds": 120,
+      "suites": 5,
+      "ownership": "خاص",
+      "established": 2010,
+      "phone": "0910000301",
+      "email": "",
+      "website": "",
+      "parking": true,
+      "conference": false,
+      "restaurant": true,
+      "rating": 3.8,
+      "lat": 32.37,
+      "lng": 15.097,
+      "image": "",
+      "notes": "وسط المدينة"
     }
-    map.addLayer(markersCluster);
-}
+  ],
 
-// تصفية العلامات حسب الفئة
-function filterMarkers(category) {
-    markersCluster.clearLayers();
-    
-    let filteredMarkers = [];
-    if (category === 'all') {
-        filteredMarkers = allMarkers;
-    } else {
-        filteredMarkers = allMarkers.filter(m => m.category === category);
+  // المنتجعات
+  "resorts": [
+    {
+      "id": 1,
+      "name": "منتجع الأمان",
+      "name_en": "Al Aman Resort",
+      "classification": "شاطئي",
+      "chalets": 120,
+      "villas": 20,
+      "rooms_total": 150,
+      "pools": 3,
+      "beach": true,
+      "area": 0.85,
+      "ownership": "خاص",
+      "phone": "0910000500",
+      "email": "",
+      "website": "",
+      "status": "نشط",
+      "lat": 32.355,
+      "lng": 15.07,
+      "image": "",
+      "notes": "منتجع بحري"
+    },
+    {
+      "id": 2,
+      "name": "منتجع سفاري",
+      "name_en": "Safari Resort",
+      "classification": "سياحي",
+      "chalets": 90,
+      "villas": 15,
+      "rooms_total": 110,
+      "pools": 2,
+      "beach": true,
+      "area": 0.6,
+      "ownership": "خاص",
+      "phone": "0910000501",
+      "email": "",
+      "website": "",
+      "status": "نشط",
+      "lat": 32.358,
+      "lng": 15.075,
+      "image": "",
+      "notes": "أنشطة ترفيهية"
     }
-    
-    filteredMarkers.forEach(m => markersCluster.addLayer(m));
-    
-    // تحديث القائمة الجانبية
-    updateItemsList(category);
-}
-
-// تحديث القائمة الجانبية
-function updateItemsList(category) {
-    let listDiv = document.getElementById('items-list');
-    let countSpan = document.getElementById('count');
-    
-    let filtered = category === 'all' ? allMarkers : allMarkers.filter(m => m.category === category);
-    countSpan.innerText = filtered.length;
-    
-    listDiv.innerHTML = '';
-    filtered.forEach(marker => {
-        let item = marker.itemData;
-        let card = document.createElement('div');
-        card.className = 'item-card';
-        
-        let details = '';
-        if (marker.category === 'restaurants') {
-            details = `<p>📍 ${item.address || ''}</p><p>📞 ${item.phone || ''}</p>`;
-        } else if (marker.category === 'hotels') {
-            details = `<p>🏨 ${item.classification || ''}</p><p>🛏️ ${item.rooms || ''} غرفة</p><p>📞 ${item.phone || ''}</p>`;
-        } else if (marker.category === 'resorts') {
-            details = `<p>🏝️ ${item.classification || ''}</p><p>🏠 ${item.chalets || ''} شاليه</p><p>📞 ${item.phone || ''}</p>`;
-        }
-        
-        card.innerHTML = `
-            <h4>${item.name}</h4>
-            ${details}
-            <button onclick="map.flyTo([${item.lat}, ${item.lng}], 16); marker.openPopup();">🔍 عرض على الخريطة</button>
-        `;
-        listDiv.appendChild(card);
-    });
-}
-
-// ربط أزرار التصفية
-document.querySelectorAll('.filter-btn').forEach(btn => {
-    btn.addEventListener('click', function() {
-        document.querySelectorAll('.filter-btn').forEach(b => b.classList.remove('active'));
-        this.classList.add('active');
-        filterMarkers(this.dataset.category);
-    });
-});
-
-// بدء التطبيق
-window.onload = function() {
-    addMarkers();
-    filterMarkers('all');
+  ]
 };
